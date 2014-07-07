3720f9c75c6ddd2661fec153fc90f48fbeb29531
Jonathan Pearlin <jpearlin@newrelic.com>
2014-07-07 14:03:10 -0400

    Added new lines to report print out.

--- src/main/groovy/org/gradle/plugins/dependency/analyze/internal/AsciiDependencyAnalysisReportRenderer.groovy
+++ src/main/groovy/org/gradle/plugins/dependency/analyze/internal/AsciiDependencyAnalysisReportRenderer.groovy
@@ -11,19 +11,25 @@ import org.gradle.plugins.dependency.analyze.DependencyAnalysisReport
  */
 class AsciiDependencyAnalysisReportRenderer extends TextReportRenderer {
 
-	void render(DependencyAnalysisReport report) {
-		if(report.getUsedUndeclaredArtifacts()) {
-			getTextOutput().withStyle(StyledTextOutput.Style.Header).text('Used undeclared dependencies found:')
-			report.getUsedUndeclaredArtifacts().each {
-				getTextOutput().withStyle(StyledTextOutput.Style.Normal).text("\t${it}")
-			}
-		}
+    void render(DependencyAnalysisReport report) {
+        if(report.getUsedUndeclaredArtifacts()) {
+            getTextOutput().withStyle(StyledTextOutput.Style.Header).text('Used undeclared dependencies found:')
+            getTextOutput().println()
+            report.getUsedUndeclaredArtifacts().each {
+                getTextOutput().withStyle(StyledTextOutput.Style.Normal).text("\t${it}")
+                getTextOutput().println()
+            }
+        }
 
-		if(report.getUnusedDeclaredDependencies()) {
-			getTextOutput().withStyle(StyledTextOutput.Style.Header).text('Unused declared dependencies found:')
-			report.getUnusedDeclaredDependencies().each {
-				getTextOutput().withStyle(StyledTextOutput.Style.Normal).text("\t${it}")
-			}
-		}
-	}
+        if(report.getUnusedDeclaredDependencies()) {
+            getTextOutput().withStyle(StyledTextOutput.Style.Header).text('Unused declared dependencies found:')
+            getTextOutput().println()
+            report.getUnusedDeclaredDependencies().each {
+                getTextOutput().withStyle(StyledTextOutput.Style.Normal).text("\t${it}")
+                getTextOutput().println()
+            }
+        }
+
+        getTextOutput().println()
+    }
 }
