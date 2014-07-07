c7f4b7c0a294d62cfb168af7925cd3a74fe8595a
Jonathan Pearlin <jpearlin@newrelic.com>
2014-07-07 14:03:35 -0400

    Renamed tasks.

--- src/main/groovy/org/gradle/plugins/dependency/DependencyPlugin.groovy
+++ src/main/groovy/org/gradle/plugins/dependency/DependencyPlugin.groovy
@@ -8,21 +8,21 @@ import org.gradle.plugins.dependency.update.UpdateVersionsTask
 
 class DependencyPlugin implements Plugin<Project> {
 
-	@Override
-	public void apply(Project project) {
-		project.logger?.info("Applying Gradle dependency plugin to ${project.name}...")
+    @Override
+    public void apply(Project project) {
+        project.logger?.info("Applying Gradle dependency plugin to ${project.name}...")
 
-		// Create the analyze dependency task and register it with the project.
-		project.tasks.create(name: 'dependency-analyze', type: AnalyzeTask)
-		Task analyzeTask = project.tasks.getByName('dependency-analyze')
-		analyzeTask.setDependsOn(['classes'])
-		analyzeTask.setDescription('Analyzes the dependencies of this project and determines which are: used and declared; used and undeclared; unused and declared')
-		analyzeTask.setGroup('Help')
+        // Create the analyze dependency task and register it with the project.
+        project.tasks.create(name: 'analyzeDependencies', type: AnalyzeTask)
+        Task analyzeTask = project.tasks.getByName('analyzeDependencies')
+        analyzeTask.setDependsOn(['classes'])
+        analyzeTask.setDescription('Analyzes the dependencies of this project and determines which are: used and declared; used and undeclared; unused and declared')
+        analyzeTask.setGroup('Help')
 
-		// Create the update versions task and register it with the project.
-		project.tasks.create(name: 'dependency-version-update', type: UpdateVersionsTask)
-		Task updateTask = project.tasks.getByName('dependency-version-update')
-		updateTask.setDescription('Updates any matching dependencies to the latest release version.')
-		updateTask.setGroup('Help')
-	}
+        // Create the update versions task and register it with the project.
+        project.tasks.create(name: 'updateDependencyVersions', type: UpdateVersionsTask)
+        Task updateTask = project.tasks.getByName('updateDependencyVersions')
+        updateTask.setDescription('Updates any matching dependencies to the latest release version.')
+        updateTask.setGroup('Help')
+    }
 }
