35c6f2f345a01c41b753cdd0366182215759ee21
Jonathan Pearlin <jpearlin@newrelic.com>
2014-07-07 14:26:14 -0400

    Corrected object type class name regex.
    Formatting clean up.

--- src/main/groovy/org/gradle/plugins/dependency/analyze/asm/visitor/ASMVisitorResult.groovy
+++ src/main/groovy/org/gradle/plugins/dependency/analyze/asm/visitor/ASMVisitorResult.groovy
@@ -6,48 +6,48 @@ import org.objectweb.asm.Type
 
 class ASMVisitorResult {
 
-	private Set<String> dependencyClasses = new HashSet<String>()
+    private Set<String> dependencyClasses = new HashSet<String>()
 
-	void addClassName(String className) {
-		if(className) {
-			Matcher matcher = className =~ /^\[L(.+);$/
-			if(matcher.matches()) {
-				className = matcher[0][1]
-			}
-			dependencyClasses << className.replaceAll('/', '.')
-		}
-	}
+    void addClassName(String className) {
+        if(className) {
+            Matcher matcher = className =~ /^\[L(.+);$/
+            if(matcher.matches()) {
+                className = matcher[0][1]
+            }
+            dependencyClasses << className.replaceAll('/', '.')
+        }
+    }
 
-	void addClassNames(final String[] classNames) {
-		classNames?.each { String className ->
-			addClassName(className)
-		}
-	}
+    void addClassNames(final String[] classNames) {
+        classNames?.each { String className ->
+            addClassName(className)
+        }
+    }
 
-	void addDescriptor(final String descriptor) {
-		addType(Type.getType(descriptor))
-	}
+    void addDescriptor(final String descriptor) {
+        addType(Type.getType(descriptor))
+    }
 
-	void addType(final Type type) {
-		switch(type.getSort()) {
-			case Type.ARRAY:
-				addType(type.getElementType())
-				break
-			case Type.OBJECT:
-				addClassName(type.getClassName().replaceAll( '.', '/' ))
-				break
-		}
-	}
+    void addType(final Type type) {
+        switch(type.getSort()) {
+            case Type.ARRAY:
+                addType(type.getElementType())
+                break
+            case Type.OBJECT:
+                addClassName(type.getClassName().replaceAll( '\\.', '/' ))
+                break
+        }
+    }
 
-	void addMethodDescriptor(String methodDescriptor) {
-		addType(Type.getReturnType(methodDescriptor))
+    void addMethodDescriptor(String methodDescriptor) {
+        addType(Type.getReturnType(methodDescriptor))
 
-		Type.getArgumentTypes(methodDescriptor)?.each { Type type ->
-			addType(type)
-		}
-	}
+        Type.getArgumentTypes(methodDescriptor)?.each { Type type ->
+            addType(type)
+        }
+    }
 
-	Set<String> getDependencyClasses() {
-		dependencyClasses
-	}
+    Set<String> getDependencyClasses() {
+        dependencyClasses
+    }
 }
